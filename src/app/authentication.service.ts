42c9297ec9c523087a2820789769f99d37c7eb6c
caudlejohnp <caudlejohnp@gmail.com>
2020-09-11 15:31:06 -0500

    add auth and sign up features

---
+++ src/app/authentication.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private token: string;
+
+  get getToken(): string {
+    return this.token;
+  }
+
+  constructor(private apiService: ApiService) { }
+
+  async signUp(username: string, password: string): Promise<any> {
+    return await this.apiService.post('auth/signup', {username, password})
+  }
+
+  async login(username: string, password: string): Promise<void> {
+    const response = await this.apiService.post('auth/login', { username, password });
+
+    this.token = response.token;
+  }
+}
--- src/app/movie-api.service.ts
+++ src/app/movie-api.service.ts
@@ -7,7 +7,7 @@ export class MovieApiService {
   constructor(private httpClient : HttpClient) { }
   url = "https://api.themoviedb.org/3/"
   async get(path, options?): Promise<any>{
-    return await this.httpClient.get(this.url + path + "&api_key=a917e025b6c202b9d096152592a6a5ad",options).toPromise();
+    return await this.httpClient.get(this.url + path + "&api_key=742409deb22e895f67b03956c9c73622",options).toPromise();
   }
 
 }
